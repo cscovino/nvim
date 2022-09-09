09ed354aaf6a6fea9e77e223c7db1a5d236cd273
Carlos Scovino <kikescovino@gmail.com>
2022-09-09 18:54:55 -0300

    feat: setup lualine options

--- lua/lualine-config/init.lua
+++ lua/lualine-config/init.lua
@@ -1,1 +1,7 @@
-require('lualine').setup({})
+require('lualine').setup({
+  options = {
+    theme = 'gruvbox_dark',
+    component_separators = { left = '', right = '' },
+    section_separators = { left = '', right = '' },
+  },
+})
