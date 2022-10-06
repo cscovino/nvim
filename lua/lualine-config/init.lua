11014b5e8c43268662b6c2b6667e86da04f55a45
Carlos Scovino <kikescovino@gmail.com>
2022-10-06 03:01:25 -0300

    fix: lsp buf format and lualine icons

--- lua/lualine-config/init.lua
+++ lua/lualine-config/init.lua
@@ -1,9 +1,16 @@
-local custom_gruvbox = require('lualine.themes.gruvbox')
-
 require('lualine').setup({
   options = {
-    theme = custom_gruvbox,
     component_separators = { left = '', right = '' },
     section_separators = { left = '', right = '' },
+  },
+  sections = {
+    lualine_b = {
+      'branch',
+      'diff',
+      {
+        'diagnostics',
+        symbols = { error = ' ', warn = ' ', hint = 'ﯦ ', info = ' ' },
+      },
+    },
   },
 })
