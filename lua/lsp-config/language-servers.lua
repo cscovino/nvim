6d56e86aa8ad3df7601d0d096f5f321f4f57c67f
Carlos Scovino <kikescovino@gmail.com>
2022-10-06 02:01:17 -0300

    feat: diagnostics highlight only text

--- lua/lsp-config/language-servers.lua
+++ lua/lsp-config/language-servers.lua
@@ -42,5 +42,5 @@ end
 local signs = { Error = ' ', Warn = ' ', Hint = 'ﯦ ', Info = ' ' }
 for type, icon in pairs(signs) do
   local hl = 'DiagnosticSign' .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+  vim.fn.sign_define(hl, { text = icon, texthl = hl })
 end
