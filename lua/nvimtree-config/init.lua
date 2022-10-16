4f3444146a7ce337b8f7314991a2e1c7cad28af3
Carlos Scovino <kikescovino@gmail.com>
2022-10-16 15:58:29 -0300

    feat: open nvim tree on setup

--- lua/nvimtree-config/init.lua
+++ lua/nvimtree-config/init.lua
@@ -1,4 +1,13 @@
 require('nvim-tree').setup({
+  open_on_setup = true,
+  view = {
+    float = {
+      enable = true,
+    },
+  },
+  renderer = {
+    indent_markers = { enable = true },
+  },
   actions = {
     open_file = {
       quit_on_open = true,
