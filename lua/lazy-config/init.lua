38694b5935c7b3abae46c554a043463a3ab83ad3
Carlos <kikescovino@gmail.com>
2024-04-07 22:46:07 +0200

    refactor: migrate to none-ls

--- lua/lazy-config/init.lua
+++ lua/lazy-config/init.lua
@@ -62,7 +62,7 @@ require('lazy').setup({
   'nvim-treesitter/nvim-treesitter-context',
   'tpope/vim-commentary',
   'JoosepAlviste/nvim-ts-context-commentstring',
-  'jose-elias-alvarez/null-ls.nvim',
+  'nvimtools/none-ls.nvim',
   'APZelos/blamer.nvim',
   'tpope/vim-fugitive',
   'mbbill/undotree',
