b0117a6bfdfb352a4f1ee6fc25603e170b6df4a3
Carlos Scovino <kikescovino@gmail.com>
2026-03-25 14:38:13 +0100

    fix(flash): 🐛 remove visual mode from flash treesitter keymap

--- lua/plugin-manager/lazy/init.lua
+++ lua/plugin-manager/lazy/init.lua
@@ -336,7 +336,7 @@ require('lazy').setup({
         },
         {
           'S',
-          mode = { 'n', 'x', 'o' },
+          mode = { 'n', 'o' },
           function()
             require('flash').treesitter()
           end,
